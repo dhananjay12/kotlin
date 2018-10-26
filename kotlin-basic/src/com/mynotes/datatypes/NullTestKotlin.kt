963addb53501716090e6ea41a4ccdd5519b367d2
Dhananjay Singh <dhananjay1204@gmail.com>
2018-10-26 16:26:08 +0530

    Nulls

--- kotlin-basic/src/com/mynotes/datatypes/NullTestKotlin.kt
+++ kotlin-basic/src/com/mynotes/datatypes/NullTestKotlin.kt
@@ -12,4 +12,29 @@ fun main(args: Array<String>) {
 
     println(str1?.toLowerCase())
 
+    val str2: String? = null
+
+    println("When it is null=> " + str2?.toUpperCase())
+
+    val str3 = str2 ?: "Default Value"
+
+    println(str3)
+
+    val myArr: Any = arrayOf(1, 2, 3, 4)
+    val str4 = myArr as? String
+    println(str4)
+
+    // not null assertion
+    val str5: String? = "test"
+
+    val str6 = str5!!.toUpperCase()
+    println(str6)
+
+    val str7: String? = null
+
+    val str8 = str7!!.toUpperCase()
+    println(str8)
+
+
+
 }
